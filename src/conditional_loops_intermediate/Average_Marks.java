b9674d48428e895794d4ef86bf837e090180ba32
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-06 23:43:32 +0530

    Answers

--- src/conditional_loops_intermediate/Average_Marks.java
+++ src/conditional_loops_intermediate/Average_Marks.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class Average_Marks {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int count = 0;
+        double[] array = new double[count];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter input number " + (i + 1) + ": ");
+            array[i] = sc.nextDouble();
+        }
     }
-    static long average(long marks) {
+    static long average(long marks, int number) {
         int sum = 0;
         for (int i = 0; i < marks; i++) {
             sum += marks;
         }
-        for (int i = 0; i < marks; i++) {
-            
-        }
+        sum /= number;
+        return sum;
+
     }
 }
