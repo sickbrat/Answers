701804f8297421ebbd4451dc40f1de5225984232
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-10 00:00:08 +0530

    Answers

--- src/functions/Even_Odd.java
+++ src/functions/Even_Odd.java
@@ -8,15 +8,12 @@ public class Even_Odd {
 
         System.out.print("Enter an Integer: ");
         int a = sc.nextInt();
-        System.out.println(even_odd(a));
+        even_odd(a);
     }
-    static int even_odd(int num) {
+    static void even_odd(int num) {
 
-            if (num % 2 == 0) {
-                System.out.println(num + " is Even");
-            } else {
-                System.out.println(num + " is Odd");
-            }
-        return 1;
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else System.out.println(num + " is Odd");
     }
 }
