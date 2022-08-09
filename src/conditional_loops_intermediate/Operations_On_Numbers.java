144a1f7bf866645b2ad9692f8bb6a1a0fc7c032d
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-09 19:13:32 +0530

    Answers

--- src/conditional_loops_intermediate/Operations_On_Numbers.java
+++ src/conditional_loops_intermediate/Operations_On_Numbers.java
@@ -1,7 +1,28 @@
 package conditional_loops_intermediate;
 
+import java.util.Scanner;
+
 public class Operations_On_Numbers {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0, even_sum = 0, odd_sum = 0;
 
+        System.out.println("Enter Numbers: ");
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            if (n < 0) {
+                sum += n;
+            } else if (n % 2 == 0) {
+                even_sum += n;
+            } else {
+                odd_sum = n;
+            }
+        }
+        System.out.println("Negative Sum: " + sum);
+        System.out.println("Positive Sum: " + even_sum);
+        System.out.println("Positive Odd Sum: " + odd_sum);
     }
 }
