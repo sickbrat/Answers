591e32ae83f15729cada8af78a172af99e1d6fc9
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-09 23:52:32 +0530

    Answers

--- src/functions/Even_Odd.java
+++ src/functions/Even_Odd.java
@@ -1,7 +1,22 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Even_Odd {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter an Integer: ");
+        int a = sc.nextInt();
+        System.out.println(even_odd(a));
+    }
+    static int even_odd(int num) {
+
+            if (num % 2 == 0) {
+                System.out.println(num + " is Even");
+            } else {
+                System.out.println(num + " is Odd");
+            }
+        return 1;
     }
 }
