d59f9a8159878224bd5e86ec88b3f83776cc4a76
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-14 22:28:44 +0530

    Answers

--- src/functions/flow/Leap.java
+++ src/functions/flow/Leap.java
@@ -8,12 +8,25 @@ public class Leap {
 
         System.out.print("Enter any Year: ");
         int a = sc.nextInt();
-        System.out.println(leaf(a));
+//        System.out.println(leaf(a));
+
+        if_else(a);
     }
     static int leaf(int year) {
         if (year % 4 == 0) {
             System.out.println(year + " is a Leap Year.");
         } else System.out.println(year + " is not a Leap Year.");
         return year;
+    }
+    static void if_else(int n) {
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else if (n >= 2 && n <= 5) {
+            System.out.println("NOT");
+        } else if (n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else if (n > 20) {
+            System.out.println("nOT");
+        }
     }
 }
