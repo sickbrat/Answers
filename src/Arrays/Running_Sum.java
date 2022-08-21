c125df7c4240ce0a39f968a40bf21732e5a0187b
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-21 14:52:12 +0530

    Answers

---
+++ src/Arrays/Running_Sum.java
@@ -0,0 +1,21 @@
+// Question --> https://leetcode.com/problems/running-sum-of-1d-array/
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class Running_Sum {
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,4 };
+        System.out.println(Arrays.toString(running(arr)));
+    }
+    static int[] running(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] + nums[i];
+        }
+        return ans;
+    }
+}
