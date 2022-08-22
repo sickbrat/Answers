251427202e5d2e2bdda91e3010f238db9dedd367
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2022-08-22 14:23:07 +0530

    Answers

---
+++ src/Arrays/Richest_Customer.java
@@ -0,0 +1,24 @@
+//  Question --> https://leetcode.com/problems/richest-customer-wealth/
+
+package Arrays;
+
+public class Richest_Customer {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 5},
+                {5, 6, 7}
+        };
+        wealth(arr);
+    }
+    static void wealth(int[][] accounts) {
+        int maxi = Integer.MIN_VALUE;
+
+        for (int[] account : accounts) {
+            int sum = 0;
+            for (int i : account) {
+                sum += i;
+            }
+            maxi = Math.max(maxi, sum);
+        }
+    }
+}
